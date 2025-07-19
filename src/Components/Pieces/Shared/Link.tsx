f25994afa97cb28b0c3194468256dec20b1e94cd
ALAN-MS-SU <origin28baker@outlook.com>
2025-07-19 14:35:29 -0300

    remove test again

--- src/Components/Pieces/Shared/Link.tsx
+++ src/Components/Pieces/Shared/Link.tsx
@@ -13,7 +13,7 @@ export function LinkStyle({
     <Link
       className=" flex flex-col text-text-secondary justify-center items-center transition-colors ease-in border-2 border-secondary cursor-pointer hover:border-transparent p-3 rounded-xl bg-transparent hover:bg-secondary text-xl font-bold hover:text-text-primary w-[200px]"
       href={Href}
-      target={Black?"_blank":"_self"}
+      target={Black?"_blank":""}
       {...props}
     >
       {Text}
