f8079640f247bccd6a4bec97a7179f65622cbbe9
ALAN-MS-SU <origin28baker@outlook.com>
2025-07-19 15:12:33 -0300

    add home responsiveness

--- src/Components/Pieces/Box/Box.tsx
+++ src/Components/Pieces/Box/Box.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 export function Box({ Project, ID }: { Project: Project; ID: number }) {
   const router = useRouter();
   return (
-    <div className="group/animate w-[400px] h-[400px] hover:scale-106 transition-all cursor-pointer perspective-midrange duration-[.5s]">
+    <div className="group/animate xl:w-[390px] xl:h-[390px] md:w-[475px] md:h-[475px] sm:w-[400px] sm:h-[400px] w-[290px] h-[290px] hover:scale-106 transition-all cursor-pointer perspective-midrange duration-[.5s]">
       <div
         className="group-hover/animate:rotate-x-[10deg] transition-all duration-[.5s] shadow-lg shadow-secondary group-hover/animate:shadow-2xl"
         onClick={() => {
@@ -18,11 +18,11 @@ export function Box({ Project, ID }: { Project: Project; ID: number }) {
         <Image className="w-full h-full" src={Project.Photo} alt="Photo" />
         <div className="absolute bottom-0 h-[80px] flex justify-center items-center w-full">
           <div className="bg-secondary relative w-full transition-all ease-in h-full z-[0] opacity-100 group-hover/animate:opacity-0">
-            <h1 className="transition-all ease-in flex justify-center items-center font-semibold h-full opacity-100 text-text-primary text-3xl z-[1] group-hover/animate:opacity-0">
+            <h1 className="transition-all ease-in flex justify-center items-center font-semibold h-full opacity-100 text-text-primary lg:text-3xl md:text-xl text-lg  z-[1] group-hover/animate:opacity-0">
               {Project.Name}
             </h1>
-            <div className="relative bottom-[90%] left-[calc(100%-65px-7.5px)] flex justify-center items-center h-[65px] w-[65px] border-[4px] border-text-primary rounded-md">
-              <p className="text-3xl font-bold text-text-primary">
+            <div className="relative sm:bottom-[90%] sm:left-[calc(100%-65px-7.5px)] bottom-[80%] left-[calc(100%-50px-7.5px)] flex justify-center items-center sm:h-[65px] sm:w-[65px] h-[50px] w-[50px] border-[4px] border-text-primary rounded-md">
+              <p className="lg:text-3xl md:text-xl text-lg font-bold text-text-primary">
                 {Project.Language}
               </p>
             </div>
--- src/Components/Pieces/Box/View.tsx
+++ src/Components/Pieces/Box/View.tsx
@@ -3,7 +3,7 @@ import { Box } from "./Box";
 
 export function ViewProjects() {
   return (
-    <div className=" w-full p-10 grid-cols-3 row-auto grid justify-items-center">
+    <div className=" w-full xl:p-10 gap-y-[50px] xl:pl-4 xl:pr-4 xl:grid-cols-3 lg:pl-0 lg:pr-0 lg:p-8 lg:grid-cols-2 sm:grid-cols-1 row-auto grid justify-items-center p-0 pt-8">
       {projects.map((project, index) => {
         return <Box Project={project} key={index} ID={index} />;
       })}
