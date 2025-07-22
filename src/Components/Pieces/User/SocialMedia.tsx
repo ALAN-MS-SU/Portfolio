055b1f0a61419c3faaa62d6122fb0f8ddf4de109
ALAN-MS-SU <origin28baker@outlook.com>
2025-07-22 01:46:25 -0300

    add responsiveness for user and social section

--- src/Components/Pieces/User/SocialMedia.tsx
+++ src/Components/Pieces/User/SocialMedia.tsx
@@ -3,19 +3,13 @@ import { LinkStyle } from "../Shared/Link";
 export async function SocialMedia() {
   return (
     <section className="flex flex-col justify-center items-center gap-y-[5px] mb-[30px]">
-      <h1 className=" text-5xl font-bold relative">Social Media</h1>
+      <h1 className="lg:text-5xl sm:text-4xl text-2xl font-bold relative">Social Media</h1>
       <div className="flex flex-row justify-center items-center gap-[px]">
-        <LinkStyle
-          Href="https://www.linkedin.com/in/alan-ms-su"
-          Black          
-        >
-          <FaLinkedin className="text-[400%] text-text-primary transition-colors ease-in cursor-pointer hover:text-primary" />
+        <LinkStyle Href="https://www.linkedin.com/in/alan-ms-su" Black>
+          <FaLinkedin className="lg:text-[400%] sm:text-[300%] text-[200%] text-text-primary transition-colors ease-in cursor-pointer hover:text-primary" />
         </LinkStyle>
-        <LinkStyle
-          Href="https://github.com/ALAN-MS-SU/ALAN-MS-SU"
-          Black
-        >
-          <FaGithubSquare className="text-[400%] text-text-primary transition-colors ease-in cursor-pointer hover:text-primary" />
+        <LinkStyle Href="https://github.com/ALAN-MS-SU/ALAN-MS-SU" Black>
+          <FaGithubSquare className="lg:text-[400%] sm:text-[300%] text-[200%] text-text-primary transition-colors ease-in cursor-pointer hover:text-primary" />
         </LinkStyle>
       </div>
     </section>
--- src/Components/Pieces/User/UserSection.tsx
+++ src/Components/Pieces/User/UserSection.tsx
@@ -3,20 +3,20 @@ import { GitUser } from "@/Constants";
 export async function UserSection() {
   const { Name, PhotoUrl, Describe } = GitUser;
   return (
-    <section className="flex flex-row justify-center items-center gap-[300px] mt-[30px] animate-user">
+    <section className="flex md:flex-row flex-col justify-center items-center xl:gap-x-[300px] lg:gap-x-[200px] md:gap-x-[100px] gap-x-[50px] mt-[30px] animate-user">
       <div className="flex flex-col justify-center items-center">
         <Image
           src={PhotoUrl}
           width={300}
           height={300}
           alt="Photo"
-          className="w-[300px] h-[300px] rounded-full border-[2px] border-text-primary"
+          className="lg:w-[300px] lg:h-[300px] sm:w-[200px] sm:h-[200px] w-[150px] h-[150px] rounded-full border-[2px] border-text-primary"
         />
-        <h2 className="min-[1210px]:text-3xl min-[610px]:text-2xl text-lg mb-[20px] font-bold">
+        <h2 className="min-[1210px]:text-3xl min-[610px]:text-xl text-lg mb-[20px] font-bold text-justify">
           {Name}
         </h2>
       </div>
-      <p className="min-[610px]:text-2xl text-md w-[40%] text-justify">
+      <p className="xl:text-2xl lg:text-xl md:text-lg text-sm lg:w-[40%] md:w-[55%] w-[80%] text-justify">
         &nbsp;&nbsp;{Describe}
       </p>
     </section>
