955f7311db8604ef96e945213ac9cd6a49370e04
ALAN-MS-SU <origin28baker@outlook.com>
2026-03-11 18:16:09 -0300

    Update Main Table Stack

--- src/Components/Pieces/User/Stack.tsx
+++ src/Components/Pieces/User/Stack.tsx
@@ -8,7 +8,7 @@ export function Stack(){
     return (
         <div className={"flex xl:flex-row flex-col items-center justify-around px-2 py-10"}>
             <h1 className="xl:text-6xl text-4xl mb-[20px] xl:w-1/3 w-full font-bold text-center">Stack Principal</h1>
-            <div className={"md:w-2/3 w-full xl:h-75 h-70  @container"}>
+            <div className={"xl:w-2/5 md:w-3/4 w-full xl:h-75 h-70  @container"}>
                 <Table Technologies={UserStack}/>
             </div>
         </div>
